9cebdd5239ad905f8ec92275fa8d784d32ce7e76
y-rockmandal105 <59593141+y-rockmandal105@users.noreply.github.com>
2020-01-07 10:38:19 +0545

    Create work.py

---
+++ work.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import os
+import time
+
+useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/74.0.3729.157 Safari/537.36"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-popup-blocking")
+chrome_options.add_argument("--allow-popups-during-page-unload")
+chrome_options.add_argument("--disable-background-timer-throttling")
+chrome_options.add_argument(f'user-agent={useragent}')
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
+driver.get("https://f1my-first.blogspot.com/")
+
+driver.find_element_by_link_text("Hello World").click()
+time.sleep(5)
+driver.find_element_by_link_text("tuned").click()
+print("ON...")
+time.sleep(1700)
+
+driver.quit()
+
+print("Great !")
